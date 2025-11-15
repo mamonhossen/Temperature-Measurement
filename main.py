f41457a15f615ac71244900b50a68ce805c2a25d
Mamon Hossen <mamonsumonk@.com>
2025-11-15 14:32:35 +0600

    weather

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from project1!")
+import requests
 
+def get_weather(latitude, longitude):
+    response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m")
+    data = response.json()
+    return data['current']['temperature_2m']
 
-if __name__ == "__main__":
-    main()
+# Get temperature for different cities
+paris_temp = get_weather(48.85, 2.35)
+london_temp = get_weather(51.50, -0.12)
+tokyo_temp = get_weather(35.68, 139.69)
+netrakona_temp = get_weather(24.88, 90.73)
+print(f"Paris: {paris_temp}°C")
+print(f"London: {london_temp}°C")
+print(f"Tokyo: {tokyo_temp}°C")
+print(f"Netrakona: {netrakona_temp}°C")
